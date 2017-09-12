634290f8756e455b3927fd0ff469908a422e3ec6
annam21 <anna.moeller@umontana.edu>
2017-09-12 15:45:54 -0600

    Day 1 plan

---
+++ Data types.R
@@ -0,0 +1,23 @@
+  # R object classes 
+  # Anna Moeller
+  # 9/12/2017
+
+  # Vectors
+  a <- c(1, 5, 235, pi)
+  b <- c("A", "B", "f", "gorilla")
+  c <- as.factor(seq(1:4))
+  d <- as.Date(c("2017-04-05", "2017-04-09", "2017-04-01", "2017-03-05"))
+  is.female <- c(T, F, T, T)
+
+  # Data.frame
+  dat <- data.frame(weight = a, 
+                    name = b, 
+                    finisher = c,
+                    date = d,
+                    is.female = is.female)
+  
+  # Examples with vectors
+  a
+  a + 3
+  a + c(1, 2, 1, 2)
+  a + c(4, 4, 4) # Warning
