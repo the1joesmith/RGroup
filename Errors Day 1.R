81368e13f8f9c9647bc7ed35aee994cffb834bb1
annam21 <anna.moeller@umontana.edu>
2017-09-12 12:37:22 -0600

    Ideas for Day 1

---
+++ Errors Day 1.R
@@ -0,0 +1,21 @@
+  # R Group errors
+  # Anna Moeller
+  # 9/11/2017
+
+  # Object doesn't exist error
+  a <- c(1, 4, 6, 2, 6)
+  a + b
+  
+  # Class mismatch error
+  b <- c("Anna", "Jenny", "Molly", "Kelsey")
+  a + b
+  
+  # Punctuation errors
+  head(a))
+  head(a, )
+  
+  c <- NULL
+  for(i in 1:length(a){
+    c[i] <- a[i] + 3
+  }
+  
---
+++ Manipulating ugly data.R
@@ -0,0 +1,10 @@
+  # Ugly data
+
+  # Packages
+  library(tidyverse)
+  
+  # Import excel spreadsheet
+  
+  # Look at your object
+  
+  # What could be wrong that we should fix? 
