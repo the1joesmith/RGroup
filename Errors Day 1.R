7eb9db7ee409392bbb009a93cf9d4ccfaa1c9454
annam21 <anna.moeller@umontana.edu>
2017-11-02 16:09:04 -0600

    errors and warnings

--- Errors Day 1.R
+++ Errors Day 1.R
@@ -12,10 +12,35 @@
   
   # Punctuation errors
   head(a))
-  head(a, )
   
   c <- NULL
   for(i in 1:length(a){
     c[i] <- a[i] + 3
   }
+  
+  # Package overwriting
+  library(dplyr)
+  library(raster)
+  
+  xx <- iris %>%
+    select(Sepal.Length, Species)
+  
+  # Fix packages
+  detach("package:dplyr", unload = T)
+  detach("package:raster", unload = T)
+
+  # More intense versions
+  r <- raster(1:100, nrow = 5)
+  ?raster
+  r <- raster(matrix(1:100, nrow = 5))
+  plot(r)
+  
+  # What other things can we do when creating our raster? 
+  # Read help file to find additional arguments
+  mat <- matrix(1:100, nrow = 5)
+  ?raster
+  r <- raster(mat, xmx = 0.5)
+  
+  # Write your own error message
+  # stopifnot, stop, warning, assert_that/see_if/validate_that
   
