3f3ed4baa79b1de806f8257f34772a889ff0eb0c
annam21 <anna.moeller@umontana.edu>
2017-11-09 14:03:41 -0700

    Error messages code and notes

--- Error Messages with Answers.R
+++ Error Messages with Answers.R
@@ -1,26 +1,20 @@
-  # R Group errors
+  # R Group errors - with answers
   # Anna Moeller
   # 9/11/2017
-
-  # Set working directory
-  setwd("/Users/annamoeller/GitHub/RGroup") # Mac
-  setwd("C:/Users/anna.moeller/Documents/GitHub") # PC
-  
-  # Most common problems: 
-  # typos, class mismatches, forgetting to run something, overwriting objects/functions,
-  # not matching function requirements
   
   # Most common R errors
   # http://blog.revolutionanalytics.com/2015/03/the-most-common-r-error-messages.html
-  # "could not find function" errors: usually caused by ______, _______, and _______
+  # "could not find function" errors, usually caused by typos or not loading a required package
   # "Error in if" errors, caused by non-logical data or missing values passed to R's "if" conditional statement
-  # "Error in eval"/object not found errors, usually caused by _______ and ________ 
-  # "cannot open" errors, usually caused by _______
-  read.csv("tidyverse/R_group_formatting")
-  # "no applicable method" errors, usually caused by ________
+  # "Error in eval" errors, caused by references to objects that don't exist
+  # "cannot open" errors, caused by attempts to read a file that doesn't exist or can't be accessed
+  # "no applicable method" errors, caused by using an object-oriented function on a data type it doesn't support
   # "subscript out of bounds" errors, caused by trying to access an element or dimension that doesn't exist
   # package errors caused by being unable to install, compile or load a package.
-
+  
+  # Most common problems: 
+  # typos, class mismatches, forgetting to run something, overwriting objects/functions
+  
   # Object not found error
   a <- c(1, 4, 6, 2, 6)
   a + b
@@ -53,7 +47,7 @@
   # Fix packages
   detach("package:dplyr", unload = T)
   detach("package:raster", unload = T)
-
+  
   # More intense versions
   r <- raster(1:100, nrow = 5)
   ?raster
@@ -71,40 +65,23 @@
   # All work inside functions, which is a great reason for using functions!
   
   # stopifnot creates its own error message for you.
-
-  
-  
-  
+  my.fn <- function(x){
+    # x must be numeric
+    
+    
+    x + 5
+  }
+  my.fn(c(1, 1, 23, 4, "b"))
   
   # With stop, you can create your own error message
-
-  
-  
-  
-  
+  my.fn <- function(x){
+    # x must be numeric
+    
+    
+    x + 5
+  }
   my.fn(c(1, 1, 23, 4))
   my.fn(c(1, 1, 23, 4, "b"))
   
   # Try creating a warning 
-    
-  
-  
-  
-  
-  # assertthat
-  # https://github.com/hadley/assertthat
-  library(assertthat)
-  x <- "RGroup"
-  
-  assert_that(is.numeric(x))
-  see_if(is.numeric(x))
-  validate_that(is.numeric(x))
-  
-  tst <- assert_that(is.numeric(x))
-  tst 
-  class(tst)
-  
-  tst <- see_if(is.numeric(x))
-  tst <- validate_that(is.numeric(x))
-
   
