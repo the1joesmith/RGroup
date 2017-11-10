6efdfc3887e065578ff18cc629aaf4a1ea89604c
annam21 <anna.moeller@umontana.edu>
2017-11-09 17:19:48 -0700

    Error messages class

--- Error messages/Error Messages.R
+++ Error messages/Error Messages.R
@@ -7,20 +7,33 @@
   setwd("C:/Users/anna.moeller/Documents/GitHub") # PC
   
   # Most common problems: 
-  # typos, class mismatches, forgetting to run something, overwriting objects/functions,
+  # typos, 
+  # punctuation error,
+  # class mismatches, 
+  # forgetting to run something, 
+  # overwriting objects/functions,
   # not matching function requirements
-  
-  # Most common R errors
+
   # http://blog.revolutionanalytics.com/2015/03/the-most-common-r-error-messages.html
-  # "could not find function" errors: usually caused by ______, _______, and _______
+  # "could not find function" errors, usually caused by typos or not loading a required package
   # "Error in if" errors, caused by non-logical data or missing values passed to R's "if" conditional statement
-  # "Error in eval"/object not found errors, usually caused by _______ and ________ 
-  # "cannot open" errors, usually caused by _______
-  read.csv("tidyverse/R_group_formatting")
-  # "no applicable method" errors, usually caused by ________
+  # "Error in eval" errors, caused by references to objects that don't exist
+  # "cannot open" errors, caused by attempts to read a file that doesn't exist or can't be accessed
+  read.csv("tidyverse/R_group_formatting.csv")
+  # "no applicable method" errors, caused by using an object-oriented function on a data type it doesn't support
   # "subscript out of bounds" errors, caused by trying to access an element or dimension that doesn't exist
   # package errors caused by being unable to install, compile or load a package.
-
+  
+  ### Tangent: save vs. saveRDS
+  x <- 1:10
+  save(x, file = "RGroup/testx.RData")
+  load("RGroup/testx.RData")
+  
+  saveRDS(x, "RGroup/testx.rds")
+  newobj <- readRDS("RGroup/testx.rds")
+  
+  ### End tangent
+  
   # Object not found error
   a <- c(1, 4, 6, 2, 6)
   a + b
@@ -50,6 +63,10 @@
   xx <- iris %>%
     select(Sepal.Length, Species)
   
+  # Fix
+  xx <- iris %>%
+    dplyr::select(Sepal.Length, Species)
+  
   # Fix packages
   detach("package:dplyr", unload = T)
   detach("package:raster", unload = T)
@@ -59,6 +76,12 @@
   ?raster
   r <- raster(matrix(1:100, nrow = 5))
   plot(r)
+  
+  ### Tangent:  S4 objects have attributes in slots
+  str(r)
+  slotNames(r)
+  r@extent@xmin
+  ### End tangent
   
   # What other things can we do when creating our raster? 
   # Read help file to find additional arguments
@@ -71,25 +94,21 @@
   # All work inside functions, which is a great reason for using functions!
   
   # stopifnot creates its own error message for you.
-
-  
-  
-  
-  
-  # With stop, you can create your own error message
-
-  
-  
-  
-  
-  my.fn(c(1, 1, 23, 4))
-  my.fn(c(1, 1, 23, 4, "b"))
-  
-  # Try creating a warning 
-    
-  
-  
-  
+  # Write your own with stop() and warning()
+  my.fn <- function(x){
+    # takes x: a length > 0 numeric input
+    if(!is.numeric(x)){
+      stop("x isn't numeric")
+    }
+    stopifnot(length(x) > 0)
+    if(any(is.na(x))){
+      warning("You fed this an NA")
+    }
+    x + 5
+  }
+  my.fn(2)
+  my.fn("b")
+  my.fn(as.numeric(c(2, NA)))
   
   # assertthat
   # https://github.com/hadley/assertthat
@@ -107,4 +126,19 @@
   tst <- see_if(is.numeric(x))
   tst <- validate_that(is.numeric(x))
 
+  
+  my.fn <- function(x, y){
+    assert_that(is.numeric(x))
+    assert_that(is.numeric(y))
+    x + y
+  }
+  
+  on_failure(my.fn) <- function(call, env) {
+    paste0("The value of x is ", deparse(call$x),
+           "The value of y is ", deparse(call$y))
+  }
+  # This doesn't work yet, but we'll try to fix it. 
+  
+  out <- my.fn(2, 5)
+  out2 <- my.fn(2, "b")
   
